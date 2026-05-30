b3989360e356324f2bc8887ba937666166510e8b
aurora-irkutsk <artemartem12936@gmail.com>
2026-05-30 23:05:23 +0800

    initWhatsAppButtonTracking

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initScrollToTop();
     initClientLeadFormTracking();
     initMasterLeadFormTracking();
-    initTelegramButtonTracking();
+    initWhatsAppButtonTracking();
     initFAQ();
 });
 
